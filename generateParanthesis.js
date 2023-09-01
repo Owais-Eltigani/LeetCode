3b9dd03849e5709102e9548cb6bc9cdab3736e44
Owais-Eltijani <ambidextrous.in@gmail.com>
2023-09-01 08:49:27 +0530

    solved generate paranthesis

--- generateParanthesis.js
+++ generateParanthesis.js
@@ -1,13 +1,34 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
+function genratePara(stack, open, closed, limit, result = []) {
+  if (open == limit && closed == limit) {
+    result = [...result, stack.join('')];
+
+    return result;
+  }
+
+  if (open < limit) {
+    stack.push('(');
+    result = genratePara(stack, open + 1, closed, limit, result);
+    // stack.pop();
+  }
+
+  if (closed < open) {
+    stack.push(')');
+    result = genratePara(stack, open, closed + 1, limit, result);
+    // stack.pop();
+  }
+
+  return result;
+}
+
 // ------------------- *** -------------------
 
-const str = String(prompt());
-
-const arr = str.split(' ');
+let n = Number(prompt());
 
 const stack = [];
 
-for (let i = 0; i < arr.length; i++) {}
+const result = genratePara(stack, 0, 0, n);
 
-console.log(stack[0]);
+console.log(result);
+console.log(result.length);
