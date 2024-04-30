c50ed2ac1790ed903c00f2fe00ffa6d84b21b2d7
Owais-Eltijani <ambidextrous.in@gmail.com>
2024-04-30 08:23:33 +0530

    solved k frequent elements

--- K_frequent_element.py
+++ K_frequent_element.py
@@ -1,8 +1,13 @@
-""" given an array """
+"""
+given an array with duplicated elements and integer k donates
+the number of returns. return the most k frequent elements 
+"""
 
 
 def find_biggest(arr: list[int]):
-    """test"""
+    """
+    find the biggest element in array
+    """
 
     biggest = 0
 
@@ -14,11 +19,18 @@ def find_biggest(arr: list[int]):
 
 
 def k_frequent(nums, k):
-    """test"""
+    """
+    using a bucket like techniques to find the best element in the array then declaring
+    another array with length k to initialized with 0s representing the occurrences
+    runs on O(n + k * m) where:
+    n := array given
+    k := number of k elements to be returned
+    m := number of elements in the frequency array which depends on the biggest element in nums
+    """
 
     max_element = max(nums)
 
-    arr = [0] * (max_element + 1)
+    arr = [0] * max_element + 1
 
     for num in nums:
         arr[num] += 1
@@ -33,7 +45,32 @@ def k_frequent(nums, k):
     return biggest
 
 
+def solve(nums, k):
+    """
+    create a dictionary then map the values by there occurrences [1,1,1,2,2,3] -> {1:3, 2:2, 3:1}
+    then creating a frequency array mapping the values of dict to there indices
+    has O(n)
+    """
+    count = {}
+    frequency = [[] for _ in range(len(nums) + 1)]
+
+    for n in nums:
+        count[n] = 1 + count.get(n, 0)
+
+    for n, c in count.items():
+        frequency[c].append(n)
+
+    k_elements = []
+
+    for i in range(len(frequency) - 1, 0, -1):
+        for n in frequency[i]:
+            if len(k_elements) != k:
+                k_elements.append(n)
+
+    return k_elements
+
+
 nums_arr = list(map(int, input().split()))
 t = int(input())
 
-print(k_frequent(nums_arr, t))
+print(solve(nums_arr, t))
